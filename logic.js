fff65acd44f1089ae459e465d5d62e9e068b0655
jarodpeters <108842304+jarodpeters@users.noreply.github.com>
2022-09-28 20:57:28 -0500

    Add files via upload
    13.5.4

--- logic.js
+++ logic.js
@@ -1,13 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -16,74 +11,39 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Get data from cities.js
-let cityData = cities;
-
-// An array containing each city's location, state, and population.
-let cities = [{
-  location: [40.7128, -74.0059],
-  city: "New York City",
-  state: "NY",
-  population: 8398748
-},
-{
-  location: [41.8781, -87.6298],
-  city: "Chicago",
-  state: "IL",
-  population: 2705994
-},
-{
-  location: [29.7604, -95.3698],
-  city: "Houston",
-  state: "TX",
-  population: 2325502
-},
-{
-  location: [34.0522, -118.2437],
-  city: "Los Angeles",
-  state: "CA",
-  population: 3990456
-},
-{
-  location: [33.4484, -112.0740],
-  city: "Phoenix",
-  state: "AZ",
-  population: 1660272
-}
-];
-
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-  console.log(city)
- });
-
- // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-  console.log(city)
-  L.marker(city.location).addTo(map);
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
 });
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.marker(city.location)
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  radius: city.population/100000
-.addTo(map);
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
 });
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// 'streets' tile layer
+streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/jarodpeters/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
